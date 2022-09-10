db0a3cbe55a539d3ae176821f34f7c505326be55
Kevin van der Burgt <kevin@zdev.com>
2022-09-10 12:08:25 +0200

    feat(water-pulse-meter): pulse detection added

--- src/modules/water-pulse-meter/water-pulse-meter.service.ts
+++ src/modules/water-pulse-meter/water-pulse-meter.service.ts
@@ -3,21 +3,35 @@ import rpio, { PULL_UP } from 'rpio';
 
 @Injectable()
 export class WaterPulseMeterService {
-  private readonly liters: number = 0;
+  private liters = 0;
 
   constructor() {
     // Setup the gpio pins
     rpio.init({
-      mock: 'raspi-b',
+      // mock: 'raspi-b',
       close_on_exit: true,
     });
 
     rpio.open(16, rpio.INPUT, PULL_UP);
 
-    //
+    rpio.poll(16, this.poll, rpio.POLL_LOW);
+  }
+
+  async poll(pin: number): Promise<void> {
+    rpio.msleep(20);
+
+    if (rpio.read(pin)) {
+      return;
+    }
+
+    this.pulse();
   }
 
   async pulse(): Promise<void> {
     // Increase the liter usage
+    this.liters++;
+
+    // eslint-disable-next-line no-console
+    console.log(`Liters: ${this.liters}`);
   }
 }
